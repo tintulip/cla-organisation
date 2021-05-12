105f51cd4eeca2b0512959e35736ca7ed0f2cfb0
Foo '0xf00' Meden <fmeden+tintulip@thoughtworks.com>
2021-05-12 17:01:46 +0100

    #27 enh: add root and log archive accounts as GD members
      Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- delegated-admin/guard_duty.tf
+++ delegated-admin/guard_duty.tf
@@ -6,3 +6,15 @@ resource "aws_guardduty_organization_configuration" "cla_guard_duty_org" {
   detector_id = data.aws_guardduty_detector.audit_detector_enabled_by_delegation.id
 }
 
+resource "aws_guardduty_member" "root" {
+  account_id         = "048191938814"
+  detector_id        = data.aws_guardduty_detector.audit_detector_enabled_by_delegation.id
+  email              = "james.gumbley@cabinetoffice.gov.uk"
+  invite             = false
+}
+resource "aws_guardduty_member" "log_archive" {
+  account_id         = "689141309029"
+  detector_id        = data.aws_guardduty_detector.audit_detector_enabled_by_delegation.id
+  email              = "fmeden+tintulip+awslogs@thoughtworks.com"
+  invite             = false
+}
--- root-delegation/guard_duty.tf
+++ root-delegation/guard_duty.tf
@@ -1,3 +1,7 @@
+resource "aws_guardduty_detector" "detector" {
+  enable = true
+}
+
 resource "aws_guardduty_organization_admin_account" "root_delegates_to_audit" {
   admin_account_id = var.audit_account_id
 }
