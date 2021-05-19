192eab2c07fd1db6c80339e1aea3c26c3e64b291
Meissane-tintulip <meissane.chami+tintulip@thoughtworks.com>
2021-05-19 17:07:53 +0100

    #36 Add ACL restriction to app log s3 bucket
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
    Co-Authored-By: Yusra Dahir (Tin Tulip) <yusra.dahir+tintulip@thoughtworks.com>

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "aws_s3_bucket" "app_logs" {
   #checkov:skip=CKV_AWS_18:Access logging needs to go into a cross account bucket
   #checkov:skip=CKV_AWS_52:Can not have mfa delete enabled only root can
 
-
   bucket = "cla-app-logs"
   acl    = "private"
 
@@ -22,14 +21,24 @@ resource "aws_s3_bucket" "app_logs" {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
-
       }
     }
   }
+
 }
-resource "aws_s3_bucket_policy" "app_logs" {
+
+resource "aws_s3_bucket_public_access_block" "app_logs" {
   bucket = aws_s3_bucket.app_logs.id
 
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+
+}
+
+resource "aws_s3_bucket_policy" "app_logs" {
+  bucket = aws_s3_bucket.app_logs.id
   policy = data.aws_iam_policy_document.cla_app_logs_role_policy.json
 }
 
