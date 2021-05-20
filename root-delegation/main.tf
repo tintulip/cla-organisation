1238fc3ef26806ff35432450b7afbeb30662cf81
Meissane-tintulip <meissane.chami+tintulip@thoughtworks.com>
2021-05-20 10:53:05 +0100

    #47 Enable GuardDuty in all regions
    It is not yet possible in terraform to enable guard duty on multiple
    regions with one statement.
    It is also not posible to use for_each on
    module providers as per the following issue: https://github.com/hashicorp/terraform/issues/24476
    It is not possible to enable GuardDuty on regions disabled by default that require opt in.

--- root-delegation/main.tf
+++ root-delegation/main.tf
@@ -1,8 +1,96 @@
-module "eu-west-2" {
+module "us-east-1" {
   source           = "./guardduty"
   audit_account_id = var.audit_account_id
   providers = {
-    aws = aws.eu-west-2
+    aws = aws.us-east-1
+  }
+}
+
+module "us-east-2" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.us-east-2
+  }
+}
+
+module "us-west-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.us-west-1
+  }
+}
+
+module "us-west-2" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.us-west-2
+  }
+}
+
+module "ap-south-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-south-1
+  }
+}
+
+module "ap-northeast-3" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-northeast-3
+  }
+}
+
+module "ap-northeast-2" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-northeast-2
+  }
+}
+
+module "ap-southeast-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-southeast-1
+  }
+}
+
+module "ap-southeast-2" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-southeast-2
+  }
+}
+
+module "ap-northeast-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ap-northeast-1
+  }
+}
+
+module "ca-central-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.ca-central-1
+  }
+}
+
+module "eu-central-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.eu-central-1
   }
 }
 
@@ -11,5 +99,37 @@ module "eu-west-1" {
   audit_account_id = var.audit_account_id
   providers = {
     aws = aws.eu-west-1
+  }
+}
+
+module "eu-west-2" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.eu-west-2
+  }
+}
+
+module "eu-west-3" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.eu-west-3
+  }
+}
+
+module "eu-north-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.eu-north-1
+  }
+}
+
+module "sa-east-1" {
+  source           = "./guardduty"
+  audit_account_id = var.audit_account_id
+  providers = {
+    aws = aws.sa-east-1
   }
 }
--- root-delegation/provider.tf
+++ root-delegation/provider.tf
@@ -1,9 +1,102 @@
+# us-east-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+# us-east-2 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+# us-west-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+}
+
+# us-west-2 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+
+# ap-south-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-south-1"
+  region = "ap-south-1"
+}
+
+# ap-northeast-3 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-northeast-3"
+  region = "ap-northeast-3"
+}
+
+# ap-northeast-2 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-northeast-2"
+  region = "ap-northeast-2"
+}
+
+
+# ap-southeast-2 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-southeast-2"
+  region = "ap-southeast-2"
+}
+
+# ap-southeast-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
+
+# ap-northeast-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ap-northeast-1"
+  region = "ap-northeast-1"
+}
+
+# ca-central-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "ca-central-1"
+  region = "ca-central-1"
+}
+
+# eu-central-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+# eu-west-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "eu-west-1"
+  region = "eu-west-1"
+}
+
+# eu-west-2 --------------------------------------------------------------------------
 provider "aws" {
   alias  = "eu-west-2"
   region = "eu-west-2"
 }
 
+# eu-west-3 --------------------------------------------------------------------------
 provider "aws" {
-  alias  = "eu-west-1"
-  region = "eu-west-1"
+  alias  = "eu-west-3"
+  region = "eu-west-3"
+}
+
+# eu-north-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "eu-north-1"
+  region = "eu-north-1"
+}
+
+# sa-east-1 --------------------------------------------------------------------------
+provider "aws" {
+  alias  = "sa-east-1"
+  region = "sa-east-1"
 }
