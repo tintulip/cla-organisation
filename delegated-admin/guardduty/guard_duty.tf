34dcc82be41be16b7d6b36cca5cf55c31d23cc61
Meissane-tintulip <meissane.chami+tintulip@thoughtworks.com>
2021-05-20 14:43:45 +0100

    #47 Disable sns to enable guard duty on all regions

--- delegated-admin/guardduty/guard_duty.tf
+++ delegated-admin/guardduty/guard_duty.tf
@@ -18,30 +18,30 @@ resource "aws_guardduty_member" "log_archive" {
   email       = "fmeden+tintulip+awslogs@thoughtworks.com"
   invite      = false
 }
-
-resource "aws_cloudwatch_event_rule" "guard_duty_findings" {
-  name        = "capture-guard-duty-findings"
-  description = "Captures GuardDuty findings"
-
-  event_pattern = <<EOF
-{
-  "source": [
-    "aws.guardduty"
-  ],
-  "detail-type": [
-    "GuardDuty Finding"
-  ]
-}
-EOF
-}
-
-# created by ControlTower
-data "aws_sns_topic" "aggregated_security_notifications" {
-  name = "aws-controltower-AggregateSecurityNotifications"
-}
-
-resource "aws_cloudwatch_event_target" "guard_duty_to_aggregated_security" {
-  rule      = aws_cloudwatch_event_rule.guard_duty_findings.name
-  target_id = "SendToSNS"
-  arn       = data.aws_sns_topic.aggregated_security_notifications.arn
-}
+//
+//resource "aws_cloudwatch_event_rule" "guard_duty_findings" {
+//  name        = "capture-guard-duty-findings"
+//  description = "Captures GuardDuty findings"
+//
+//  event_pattern = <<EOF
+//{
+//  "source": [
+//    "aws.guardduty"
+//  ],
+//  "detail-type": [
+//    "GuardDuty Finding"
+//  ]
+//}
+//EOF
+//}
+//
+//# created by ControlTower
+//data "aws_sns_topic" "aggregated_security_notifications" {
+//  name = "aws-controltower-AggregateSecurityNotifications"
+//}
+//
+//resource "aws_cloudwatch_event_target" "guard_duty_to_aggregated_security" {
+//  rule      = aws_cloudwatch_event_rule.guard_duty_findings.name
+//  target_id = "SendToSNS"
+//  arn       = data.aws_sns_topic.aggregated_security_notifications.arn
+//}
