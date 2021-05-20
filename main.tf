b57577046aa8383b2a383f1664cff5b3878a07c3
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-05-20 17:18:24 +0100

    #36 Change the type of encryption used for the logs bucket
    Co-authored-by: Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
    Co-authored-by: Meissane Chami <meissane.chami+tintulip@thoughtworks.com>

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket" "app_logs" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        sse_algorithm = "aws:kms"
       }
     }
   }
